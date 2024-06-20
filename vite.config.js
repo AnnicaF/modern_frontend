aa6cc22982b6a458ea086abc3f08281b5ba0fcfa
Annica <annif011098@gmail.com>
2024-06-20 09:40:33 +0200

    add vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import { generateSW } from "workbox-build";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      registerType: "autoUpdate",
       manifest: {
         name: "Geografisk Have",
         short_name: "GeoHave",
@@ -26,8 +28,27 @@ export default defineConfig({
         ],
       },
       workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
+        globPatterns: ["**/*.{html,js,css,png,svg,webp,}"],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:html|js|css|png|webp|svg)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "static-assets",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: /api\/.*$/,
+            handler: "NetworkOnly",
+            options: {
+              cacheName: "api-cache",
+            },
+          },
+        ],
       },
     }),
   ],
