411c27abb8fe2c325a662c195a4aa8b5abd9416e
Annica <annif011098@gmail.com>
2024-06-21 07:40:30 +0200

    works

--- src/components/InstallApp.vue
+++ src/components/InstallApp.vue
@@ -5,23 +5,41 @@ const deferredPrompt = ref(null);
 const showDownloadPrompt = ref(true);
 const showMiniBox = ref(false);
 
+// Håndter 'beforeinstallprompt' begivenhed
 const handleBeforeInstallPrompt = (e) => {
   e.preventDefault();
   deferredPrompt.value = e;
+  showDownloadPrompt.value = true; // Vis prompten, når begivenheden er udløst
+};
+
+// Håndter 'appinstalled' begivenhed
+const handleAppInstalled = () => {
+  showDownloadPrompt.value = false; // Skjul prompten, når appen er installeret
+  showMiniBox.value = false; // Skjul mini-boksen, når appen er installeret
 };
 
 onMounted(() => {
   window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  window.addEventListener("appinstalled", handleAppInstalled);
 });
 
 onUnmounted(() => {
   window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  window.removeEventListener("appinstalled", handleAppInstalled);
 });
 
 const installApp = () => {
   if (deferredPrompt.value) {
     deferredPrompt.value.prompt();
-    deferredPrompt.value = null;
+    deferredPrompt.value.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+        showDownloadPrompt.value = false; // Skjul prompten efter installation
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPrompt.value = null;
+    });
   }
 };
 
@@ -52,10 +70,11 @@ const reopenDownloadPrompt = () => {
       class="install-app__mini-box"
       @click="reopenDownloadPrompt"
     >
-      <span class="install-app__mini-text">DOWNLOADE SOM APP</span>
+      <span class="install-app__mini-text">DOWNLOAD SOM APP</span>
     </div>
   </div>
 </template>
+
 <style scoped lang="scss">
 .install-app {
   position: fixed;
@@ -66,7 +85,6 @@ const reopenDownloadPrompt = () => {
   border-radius: 5px;
   text-align: center;
   z-index: 1000;
-
   &__close-button {
     position: absolute;
     top: 5px;
@@ -77,16 +95,13 @@ const reopenDownloadPrompt = () => {
     background: none;
     border: none;
   }
-
   &__close-button:hover {
     color: red;
   }
-
   &__text {
     padding-top: 20px;
     color: white;
   }
-
   &__button {
     margin-top: 10px;
     padding: 10px 20px;
@@ -96,11 +111,9 @@ const reopenDownloadPrompt = () => {
     border-radius: 5px;
     cursor: pointer;
   }
-
   &__button:hover {
     background-color: #399a6e;
   }
-
   &__mini-box {
     position: fixed;
     bottom: 20px;
@@ -113,7 +126,6 @@ const reopenDownloadPrompt = () => {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     z-index: 1000;
   }
-
   &__mini-text {
     font-size: 14px;
     color: white;
